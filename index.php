486a2914dddc9525e092a7d543447c56bf682512
Akash Jaiswal <akashjas1996@gmail.com>
2020-01-01 13:41:46 +0530

    header

--- index.php
+++ index.php
@@ -26,71 +26,7 @@
   <body>
   
   
-<header>
-  <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
-    <div class="container">
-      <a class="navbar-brand waves-effect" href="#" target="_blank">
-        <img src="https://images.unsplash.com/photo-1522139137660-4248e04955b8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjkyMzkxfQ" alt="Logo">
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link waves-effect" href="#" target="_blank">About us</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link waves-effect" href="#" target="_blank">Pricing</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link waves-effect" href="#" target="_blank">Team</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link waves-effect" href="#" target="_blank">Contact</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav nav-flex-icons">
-          <li class="nav-item">
-            <a href="#" class="nav-link waves-effect" target="_blank">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-          </li> 
-          <li class="nav-item">
-            <a href="#" class="nav-link waves-effect" target="_blank">
-              <i class="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link waves-effect"
-              target="_blank">
-              <i class="fab fa-github"></i>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#"
-              class="nav-link border border-light rounded waves-effect mr-2" target="_blank">
-              <i class="fas fa-envelope mr-1"></i>Newsletter
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <div class="card card-intro teal lighten-4">
-    <div class="card-body white-text rgba-black-light text-center">
-      <div class="row d-flex justify-content-center">
-        <div class="col-md-6">
-          <p class="h5 mb-2">
-            Dr. Cool
-          </p>
-          <p class="mb-0">Sit amet purus gravida quis blandit turpis cursus.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</header>
+<?php include 'inc/header.php' ?>
       
   <main id="app" class="container my-5 py-5">
     
